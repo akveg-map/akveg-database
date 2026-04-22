3f029a4cfef9d112f8a7f134042f805d81fa1c2c
Amanda Droghini <10262153+adroghini@users.noreply.github.com>
2026-04-21 17:49:37 -0800

    Refactor soil horizons table
    - Move rename, join, and select processes to helper_functions_combine.R

--- 03_data_insert/01_Combine_Tables.R
+++ 03_data_insert/01_Combine_Tables.R
@@ -140,12 +140,15 @@ config_table = tribble(
   FALSE,
   'soil_metrics.csv',
   clean_soil_metrics_data,
-  join_soil_metrics_metadata
+  join_soil_metrics_metadata,
+  "soil_horizons",
+  "14_soilhorizons.*csv",
+  FALSE,
+  'soil_horizons.csv',
+  clean_soil_horizons_data,
+  join_soil_horizons_metadata
   
 )
-  "soil_horizons",
-  'soil_horizons.csv'
-  
 
 # Create function for table processing
 process_tables <- function(input_pattern, warn_if_missing, clean_function, join_function, ...) {
@@ -156,70 +159,8 @@ process_tables <- function(input_pattern, warn_if_missing, clean_function, join_
     join_function(lookup_data)
 }
 
-# Run every job in the table at once
+# Iterate through every row in the config table and apply process_tables function
 final_results_list <- pmap(config_table, process_tables)
-
-
-# Create soil horizons table ----
-
-# Set target pattern
-target_pattern = '^14_soilhorizons.*csv'
-
-print(str_c('Processing...', target_pattern, sep = " "))
-
-# Create empty list to store files
-files_list = list()
-
-# Iterate through target paths and create file list
-for (target_path in target_paths) {
-  # Create full path
-  full_path = paste(data_folder, target_path, sep = '/')
-  # Find file and append to file list
-  files = list.files(full_path, pattern = target_pattern)
-  if (!is.na(files[1])) {
-    files_list = append(files_list, paste(full_path, files[1], sep = '/'))
-  }
-}
-
-# Create read function
-read_data_site_visit_rename = function (data_file) {
-  rename_fields = c(site_visit_code = 'site_visit_id',
-                    depth_upper_cm = 'depth_upper',
-                    depth_lower_cm = 'depth_lower',
-                    horizon_primary_code = 'horizon_primary', 
-                    horizon_suffix_1_code = 'horizon_suffix_1',
-                    horizon_suffix_2_code = 'horizon_suffix_2', 
-                    horizon_secondary_code = 'horizon_secondary',
-                    horizon_suffix_3_code = 'horizon_suffix_3', 
-                    horizon_suffix_4_code = 'horizon_suffix_4',
-                    matrix_hue_code = 'matrix_hue',
-                    nonmatrix_hue_code = 'nonmatrix_hue')
-  input_data = read_csv(data_file, show_col_types = FALSE) %>%
-    rename(any_of(rename_fields))
-  return(input_data)
-}
-
-# Read files into list
-data_list = lapply(files_list, read_data_site_visit_rename)
-
-# Combine list into data frame
-data_combine = do.call(rbind, data_list)
-
-# Join metadata tables to input table
-horizons_table = data_combine %>%
-  left_join(texture_data, by = c('texture' = 'soil_texture')) %>%
-  left_join(structure_data, by = c('structure' = 'soil_structure')) %>%
-  left_join(nonmatrix_data, by = 'nonmatrix_feature') %>%
-  rename(texture_code = soil_texture_code,
-         structure_code = soil_structure_code) %>%
-  select(site_visit_code, horizon_order, thickness_cm, depth_upper_cm, depth_lower_cm,
-         depth_extend, horizon_primary_code, horizon_suffix_1_code,
-         horizon_suffix_2_code, horizon_secondary_code,
-         horizon_suffix_3_code, horizon_suffix_4_code, texture_code,
-         clay_percent, total_coarse_fragment_percent,
-         gravel_percent, cobble_percent, stone_percent, boulder_percent,
-         structure_code, matrix_hue_code, matrix_value, matrix_chroma,
-         nonmatrix_feature_code, nonmatrix_hue_code, nonmatrix_value, nonmatrix_chroma)
 
 # Export combined data ----
 write_csv(path(data_folder, 'processed', config_table$output_path))
