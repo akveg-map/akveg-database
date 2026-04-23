ddb9ece3149269ea1f11edc4353e320ba7b2cf90
Amanda Droghini <10262153+adroghini@users.noreply.github.com>
2026-04-22 19:32:42 -0800

    Use CSV instead of tribble
    Now that similar info is used in multiple scripts, writing it once and loading it in makes more sense

--- 03_data_insert/01_Combine_Tables.R
+++ 03_data_insert/01_Combine_Tables.R
@@ -25,11 +25,7 @@ drive <- "C:"
 root_folder <- "ACCS_Work/OneDrive - University of Alaska/ACCS_Teams/Vegetation/AKVEG_Database"
 
 # Define input folders
-data_folder <- path(
-  drive,
-  root_folder,
-  "Data/Data_Plots"
-)
+data_folder <- path(drive, root_folder, "Data/Data_Plots")
 repository_folder <- path(
   drive,
   "ACCS_Work/Repositories/akveg-database"
@@ -37,87 +33,51 @@ repository_folder <- path(
 credential_folder <- path(drive, root_folder, "Credentials")
 
 # Define input files ----
-project_list <- path(
-  repository_folder,
-  "03_data_insert",
-  "List_Included_Projects.json"
-)
-queries_input <- path(
-  repository_folder,
-  "03_data_insert",
-  "queries_lookup.yaml"
-)
+project_list <- path(repository_folder, "03_data_insert", "config", "List_Included_Projects.json")
+queries_input <- path(repository_folder, "03_data_insert", "config", "queries_lookup.yaml")
+metadata_input <- path(repository_folder, "03_data_insert", "config", "tables_metadata.csv")
 
 # Define functions ----
-source(path(
-  repository_folder,
-  "03_data_insert",
-  "helper_functions_combine.R"
-))
+source(path(repository_folder, "03_data_insert", "helper_functions_combine.R"))
 
 # Connect to AKVEG database ----
 
 # Import database connection function
-connection_script <- path(
-  repository_folder,
-  "pull_functions",
-  "connect_database_postgresql.R"
-)
-source(connection_script)
+source(path(repository_folder, "pull_functions", "connect_database_postgresql.R"))
 
 # Create a connection to the AKVEG PostgreSQL database
 authentication <- path(
-  credential_folder,
-  "akveg_private_build", "authentication_akveg_private_build.csv"
+  credential_folder, "akveg_private_build",
+  "authentication_akveg_private_build.csv"
 )
 database_connection <- connect_database_postgresql(authentication)
 
-# Read in queries ----
+# Read in metadata ----
+# Read and execute SQL queries
 queries_lookup <- read_yaml(queries_input)
 lookup_data <- map(queries_lookup$queries, \(q) dbGetQuery(database_connection, q))
-
-# Define vegetation plot folders ----
+# Define vegetation plot folders
 target_paths <- fromJSON(file = project_list)$projects
 all_folders <- path(data_folder, target_paths)
-
-# Create config table to hold table-specific values and functions
-config_table <- tribble(
-  ~table_name,
-  ~input_pattern,
-  ~warn_if_missing,
-  ~output_csv,
-  ~clean_function,
-  ~join_function,
-  ~serial_key_id,
-  #---------|---------|---------|---------
-  "project", "01_project.*csv", TRUE, "projects.csv", identity, join_project_metadata, NA,
-  "site", "02_site.*csv", TRUE, "sites.csv", clean_site_data, join_site_metadata, NA,
-  "site_visit", "03_sitevisit.*csv", TRUE, "site_visits.csv", clean_visit_data, join_visit_metadata, NA,
-  "vegetation_cover", "05_vegetationcover.*csv", TRUE, "vegetation_cover.csv", clean_vegetation_data,
-  join_vegetation_metadata, "vegetation_cover_id",
-  "abiotic_top_cover", "06_abiotictopcover.*csv", FALSE, "abiotic_top_cover.csv",
-  clean_abiotic_data, join_abiotic_metadata, "abiotic_cover_id",
-  "whole_tussock_cover", "07_wholetussockcover.*csv", FALSE, "whole_tussock_cover.csv",
-  clean_tussock_data, join_tussock_metadata, "tussock_id",
-  "ground_cover", "08_groundcover.*csv", FALSE, "ground_cover.csv",
-  rename_columns, join_ground_metadata, "ground_cover_id",
-  "structural_group_cover", "09_structuralgroupcover.*csv", FALSE, "structural_group_cover.csv",
-  clean_structural_data, join_structural_metadata, "structural_cover_id",
-  "shrub_structure", "11_shrubstructure.*csv", FALSE, "shrub_structure.csv", rename_columns,
-  join_shrub_metadata, "shrub_structure_id",
-  "environment", "12_environment.*csv", FALSE, "environment.csv",
-  clean_environment_data, join_environment_metadata, "environment_id",
-  "soil_metrics", "13_soilmetrics.*csv", FALSE, "soil_metrics.csv",
-  clean_soil_metrics_data, join_soil_metrics_metadata, "soil_metric_id",
-  "soil_horizons", "14_soilhorizons.*csv", FALSE, "soil_horizons.csv",
-  clean_soil_horizons_data, join_soil_horizons_metadata, "soil_horizon_id"
-)
+# Read control table file
+config_table <- read_csv(metadata_input,
+  col_select = c(
+    "table_name", "input_pattern", "output_csv", "warn_if_missing",
+    "clean_function", "join_function", "serial_key_id",
+    "include_combine"
+  )
+) |>
+  filter(include_combine) # Drop files that aren't used in this script
 
 # Create function for table processing
 process_tables <- function(input_pattern, table_name, warn_if_missing, clean_function,
                            join_function, serial_key_id,
                            database_connection, ...) {
   message(paste0("Processing table... ", table_name))
+
+  # Convert strings to functions
+  clean_logic <- match.fun(clean_function)
+  join_logic <- match.fun(join_function)
 
   # Get table schema from AKVEG database
   column_names <- dbListFields(database_connection, table_name)
@@ -130,22 +90,16 @@ process_tables <- function(input_pattern, table_name, warn_if_missing, clean_fun
   # Process and combine individual tables
   find_files_warning(all_folders, input_pattern, show_warning = warn_if_missing) |>
     map(\(f) read_csv(f, show_col_types = FALSE)) |>
-    map(clean_function) |>
+    map(clean_logic) |>
     list_rbind() |>
-    join_function(lookup_data) |>
+    join_logic(lookup_data) |>
     select(all_of(column_names))
 }
 
 # Iterate through every row in the config table and apply process_tables function
-final_results_list <- pmap(config_table, ~ process_tables(
-  table_name          = ..1,
-  input_pattern       = ..2,
-  warn_if_missing     = ..3,
-  clean_function      = ..5,
-  join_function       = ..6,
-  serial_key_id       = ..7,
+final_results_list <- pmap(config_table, process_tables,
   database_connection = database_connection
-))
+)
 
 # Export combined data ----
 ## One table for each list item
