60fc27c4e5d3637b5035a521f120b1ef153ce7d1
Amanda Droghini <10262153+adroghini@users.noreply.github.com>
2026-04-27 15:04:24 -0800

    Execute data insertion within script
    Replace formatting of SQL strings with dbExecute and dbWriteTable

--- 03_data_insert/03_Prepare_Tables.R
+++ 03_data_insert/03_Prepare_Tables.R
@@ -14,47 +14,51 @@ library(glue)
 library(lubridate)
 library(purrr)
 library(readr)
+library(readxl)
 library(RPostgres)
 library(stringr)
 library(tidyr)
 
-# Define files & directories ----
+# Define directories ----
 drive <- "C:"
 root_folder <- "ACCS_Work/OneDrive - University of Alaska/ACCS_Teams/Vegetation/AKVEG_Database"
 data_folder <- path(drive, root_folder, "Data", "Data_Plots")
 repository_folder <- path(drive, "ACCS_Work/Repositories/akveg-database")
 
-## Database credential file
-authentication <- path(
-  drive, root_folder, "Credentials", "akveg_private_build",
-  "authentication_akveg_private_build.csv"
-)
-
-## Metadata for tables
+# Define files ----
+authentication <- path(drive, root_folder, "Credentials", "akveg_private_build",
+                       "authentication_akveg_private_build.csv")
 metadata_input <- path(repository_folder, "03_data_insert", "config", "tables_metadata.csv")
-config_table <- read_csv(metadata_input,
-  col_select = c("table_name", "combined_table_csv", "output_sql", "serial_key_id")
-)
+citations_input <- path(root_folder, "Data", "Tables_Metadata", "project_citations.xlsx")
+
+# Source functions ----
+helpers <- new.env()
+source(path(repository_folder, "03_data_insert", "helper_functions.R"), local = helpers)
+execute_sql_build <- helpers$execute_sql_build
+upload_to_database <- helpers$upload_to_database
 
 # Connect to AKVEG database ----
 source(path(repository_folder, "pull_functions", "connect_database_postgresql.R"))
 database_connection <- connect_database_postgresql(authentication)
 
-# Define core logic ----
-build_insert_statement <- function(table_name, combined_table_csv,
-                                   output_sql, serial_key_id, database_connection) {
+# Read in data ----
+config_table <- read_csv(metadata_input, col_select = c("table_name", 
+                                                        "combined_table_csv", "serial_key_id"))
+citations_original <- read_excel()
+# Prepare individual tables ---
+build_list <- function(table_name, combined_table_csv,
+                                   serial_key_id, database_connection) {
   # Get table schema from AKVEG database
   column_names <- dbListFields(database_connection, table_name)
+  
   # Remove auto-incrementing ID field for tables that use it
   if (!is.na(serial_key_id)) {
     column_names <- column_names[column_names != serial_key_id]
   }
-  sql_columns <- paste(column_names, collapse = ", ")
 
-  # Define I/O paths
+  # Define input paths
   corrected_path <- path(data_folder, "processed", "corrected", combined_table_csv)
   processed_path <- path(data_folder, "processed", combined_table_csv)
-  output_path <- path(data_folder, "sql_statements", output_sql)
 
   # Read corrected table if it exists
   if (file_exists(corrected_path)) {
@@ -64,57 +68,34 @@ build_insert_statement <- function(table_name, combined_table_csv,
     df <- read_csv(processed_path, show_col_types = FALSE)
   }
 
-  # Format table for as SQL string
-  input_sql <- df %>%
-    # Escape single quotes, and wrap characters and dates in quotes
-    mutate(across(where(~ is.character(.) | is.Date(.)), ~ str_replace_all(., "'", "''"))) %>%
-    mutate(across(where(~ is.character(.) | is.Date(.)), ~ paste0("'", ., "'"))) %>%
-    # Convert all columns to character
-    mutate(across(everything(), as.character)) |>
-    # Replace true NA with 'NULL' (numeric columns)
-    mutate(across(everything(), ~ if_else(is.na(.), "NULL", as.character(.)))) |>
-    # Replace 'NA' with 'NULL' (character columns)
-    mutate(across(everything(), ~ str_replace(., "^'NA'$", "NULL"))) %>%
-    # Combine all columns and values into a single row
-    unite(sql_row, everything(), sep = ", ") %>%
-    mutate(sql_row = paste0("(", sql_row, "),"))
-
-  # Fix the last comma to a semicolon
-  last_row <- nrow(input_sql)
-  input_sql$sql_row[last_row] <- str_replace(input_sql$sql_row[last_row], ",$", ";")
-
-  # Construct the full file
-  header <- c(
-    "-- -*- coding: utf-8 -*-",
-    "-- ---------------------------------------------------------------------------",
-    glue("-- Insert data for {table_name} table"),
-    "-- Author: Timm Nawrocki, Amanda Droghini, Alaska Center for Conservation Science",
-    glue("-- Last Updated: {Sys.Date()}"),
-    "-- Usage: Script should be executed in a PostgreSQL 16+ database.",
-    "-- ---------------------------------------------------------------------------",
-    "",
-    "-- Initialize transaction",
-    "START TRANSACTION;",
-    paste0("INSERT INTO ", table_name, " (", sql_columns, ") VALUES")
-  )
-
-  footer <- c("", "COMMIT TRANSACTION;")
-
-  full_statement <- c(header, input_sql$sql_row, footer)
-
-  # Replace NA with NULL
-  full_statement <- full_statement %>%
-    str_replace_all(", NA,", ", NULL,") %>%
-    str_replace_all(", 'NA'", ", NULL") %>%
-    str_replace_all(", 'NULL'", ", NULL")
-
-  # Write to SQL file
-  write_lines(full_statement, output_path)
+  # Ensure the dataframe only contains the columns in the database
+  df |> 
+    select(all_of(column_names))
 }
 
-# Execute logic ----
-# Iterate through every row in the config table
-pwalk(config_table, build_insert_statement, database_connection = database_connection)
+# Combine tables into list ----
+final_tables <- pmap(config_table, build_list, 
+                     database_connection = database_connection) |> 
+  set_names(config_table$table_name)
+
+# Execute SQL build ----
+## This will drop existing metadata tables and create new (empty) ones
+
+# Close any open transactions (e.g., resulting from transaction failures)
+try(DBI::dbExecute(database_connection, "ROLLBACK;"), silent = TRUE)
+
+# Define build scripts
+build_scripts <- c("03_Project.sql", "04_Site.sql", "05_Vegetation.sql", "06_Abiotic.sql", "07_Environment.sql")
+
+# Execute each build script in turn
+walk(build_scripts, execute_sql_build, 
+     database_connection = database_connection, 
+     repository_folder = repository_folder)
+
+# Upload data tables to database
+upload_to_database(database_connection, final_tables)
+
+
 
 # Close database connection
 dbDisconnect(database_connection)
