0fe585d0679f4a06ed355dce2f9bd85d3235e604
Amanda Droghini <10262153+adroghini@users.noreply.github.com>
2026-04-28 11:51:54 -0800

    Add source information to project query
    - Add source type, download URL, and acquisition date fields to project table query

--- queries/01_project.sql
+++ queries/01_project.sql
@@ -2,25 +2,29 @@
 -- ---------------------------------------------------------------------------
 -- Query projects
 -- Author: Timm Nawrocki, Alaska Center for Conservation Science
--- Last Updated:  2025-04-13
--- Usage: Script should be executed in a PostgreSQL 14+ database.
+-- Last Updated:  2026-04-28
+-- Usage: Script should be executed in a PostgreSQL 16+ database.
 -- Description: "Query projects" queries the project metadata.
 -- ---------------------------------------------------------------------------
 
 -- Compile project data
-SELECT DISTINCT site_visit.project_code as project_code
-     , project.project_name as project_name
-     , originator.organization as originator
-     , funder.organization as funder
-     , personnel.personnel as manager
-     , completion.completion as project_status
-     , project.year_start as year_start
-     , project.year_end as year_end
-     , project.project_description as project_description
-     , project.private as private
+SELECT DISTINCT site_visit.project_code     as project_code
+              , project.project_name        as project_name
+              , originator.organization     as originator
+              , funder.organization         as funder
+              , personnel.personnel         as manager
+              , completion.completion       as project_status
+              , project.year_start          as year_start
+              , project.year_end            as year_end
+              , project.project_description as project_description
+              , project.private             as private
+              , source_type.source_type     as source_type
+              , project.download_url        as source_url
+              , project.acquisition_date    as acquisition_date
 FROM site_visit
-    LEFT JOIN project ON site_visit.project_code = project.project_code
-    LEFT JOIN organization originator ON project.originator_id = originator.organization_id
-    LEFT JOIN organization funder ON project.funder_id = funder.organization_id
-    LEFT JOIN personnel ON project.manager_id = personnel.personnel_id
-    LEFT JOIN completion ON project.completion_id = completion.completion_id;
+         LEFT JOIN project ON site_visit.project_code = project.project_code
+         LEFT JOIN organization originator ON project.originator_id = originator.organization_id
+         LEFT JOIN organization funder ON project.funder_id = funder.organization_id
+         LEFT JOIN personnel ON project.manager_id = personnel.personnel_id
+         LEFT JOIN completion ON project.completion_id = completion.completion_id
+         LEFT JOIN source_type ON project.source_type_id = source_type.source_type_id;
