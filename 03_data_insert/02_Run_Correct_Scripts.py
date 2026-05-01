12941d68d6293242af2f5efd1d05dba8da3efb77
Amanda Droghini <10262153+adroghini@users.noreply.github.com>
2026-05-01 12:42:08 -0800

    Delete files in corrected directory
    Start fresh every time

--- 03_data_insert/02_Run_Correct_Scripts.py
+++ 03_data_insert/02_Run_Correct_Scripts.py
@@ -2,7 +2,7 @@
 # ---------------------------------------------------------------------------
 # Batch run 'correct' scripts
 # Author: Amanda Droghini
-# Last Updated: 2026-02-04
+# Last Updated: 2026-05-01
 # Usage: Execute in Python 3.13+.
 # Description: "Batch run 'correct' scripts" iterates through each Python script that identifies and corrects quality
 # control issues. The output is a series of CSV files that contain the corrected tables. Importantly, this
@@ -14,6 +14,7 @@
 # Import required libraries
 import subprocess
 from pathlib import Path
+from shutil import rmtree
 
 # Define Python environment
 envr = Path('C:\\Users\\adroghini\\miniforge3\\envs\\accs_akveg\\python.exe')
@@ -25,6 +26,16 @@ root_folder = 'ACCS_Work'
 # Define folder structure
 repository_folder = (drive / root_folder / 'Repositories' / 'akveg-database' / '03_data_insert' /
                      'individual_scripts')
+corrected_folder = (drive / root_folder / 'OneDrive - University of Alaska' / 'ACCS_Teams' /
+                    'Vegetation' / 'AKVEG_Database' / 'Data' / 'Data_Plots' / 'processed' / 'corrected')
+
+# Remove all files in corrected directory
+## Helps keep directory clean if old files are no longer used in/replaced by scripts
+for path in corrected_folder.glob("**/*"):
+    if path.is_file():
+        path.unlink()
+    elif path.is_dir():
+        rmtree(path)
 
 # List relevant files
 file_list = list(repository_folder.glob('[0-9]*Correct*'))
