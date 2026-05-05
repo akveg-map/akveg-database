0651008e09590576a842fb5b1a50415df42717c8
Amanda Droghini <10262153+adroghini@users.noreply.github.com>
2026-05-05 11:42:01 -0800

    Update project query
    Reflect change in schema: citations in project table now reference citations table

--- queries/01_project.sql
+++ queries/01_project.sql
@@ -2,7 +2,7 @@
 -- ---------------------------------------------------------------------------
 -- Query projects
 -- Author: Timm Nawrocki, Alaska Center for Conservation Science
--- Last Updated:  2026-04-28
+-- Last Updated:  2026-05-05
 -- Usage: Script should be executed in a PostgreSQL 16+ database.
 -- Description: "Query projects" queries the project metadata.
 -- ---------------------------------------------------------------------------
@@ -19,7 +19,8 @@ SELECT DISTINCT site_visit.project_code     as project_code
               , project.project_description as project_description
               , project.private             as private
               , source_type.source_type     as source_type
-              , project.download_url        as source_url
+              , citations.citation_short    as source_citation
+              , citations.citation_url      as source_url
               , project.acquisition_date    as acquisition_date
 FROM site_visit
          LEFT JOIN project ON site_visit.project_code = project.project_code
@@ -27,4 +28,5 @@ FROM site_visit
          LEFT JOIN organization funder ON project.funder_id = funder.organization_id
          LEFT JOIN personnel ON project.manager_id = personnel.personnel_id
          LEFT JOIN completion ON project.completion_id = completion.completion_id
-         LEFT JOIN source_type ON project.source_type_id = source_type.source_type_id;
+         LEFT JOIN source_type ON project.source_type_id = source_type.source_type_id
+         LEFT JOIN citations ON project.citation_id = citations.citation_id;
