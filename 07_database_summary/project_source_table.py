7b4f0bae11bf68865ccce29dc6b961a6c44b8f95
Amanda Droghini <10262153+adroghini@users.noreply.github.com>
2026-05-06 10:43:04 -0800

    Get unique sources
    Filter project table to extract unique sources to include in references section of the manuscript

--- 07_database_summary/project_source_table.py
+++ 07_database_summary/project_source_table.py
@@ -2,7 +2,7 @@
 # ---------------------------------------------------------------------------
 # Create project table
 # Author: Amanda Droghini, Alaska Center for Conservation Science
-# Last Updated: 2026-05-05
+# Last Updated: 2026-05-06
 # Usage: Execute in Python 3.13+.
 # Description: "Create project source table" formats the project and citations tables in the AKVEG Database to
 # produce Table 1 (Droghini et al., in prep.) and associated bibliographic references.
@@ -20,56 +20,44 @@ root = drive / 'ACCS_Work'
 project_folder = root / 'OneDrive - University of Alaska' /'ACCS_Teams' /'Vegetation' / 'AKVEG_Database'
 repository_folder = root / 'Repositories' / 'akveg-database'
 credential_folder = project_folder / "Credentials"
-
+manuscript_folder = root / 'Projects' / 'AKVEG_Database' / 'Manuscript' / 'Tables'
 # Define inputs
 akveg_credentials = (credential_folder / "akveg_private_read" / "authentication_akveg_private_read.csv")
 project_input = repository_folder / "queries" / "01_project.sql"
 
 # Define outputs
-project_output = root / 'Projects' / 'AKVEG_Database' / 'Manuscript' / 'Tables' / 'project_source.csv'
-
-# Read in queries
-with open(project_input, 'r') as file:
-    project_query = file.read()
+project_output =  manuscript_folder / 'project_table.csv'
+source_output = manuscript_folder / 'source_table.csv'
 
 # Connect to the AKVEG Database
 akveg_db_connection = connect_database_postgresql(akveg_credentials)
 
-# Create cursor
-cursor = akveg_db_connection.cursor()
+# Read in queries
+with open(project_input, 'r') as file:
+    project_query = file.read()
 
 # Query project table
 project_table = query_to_dataframe(akveg_db_connection, project_query)
 project_table = pl.from_pandas(project_table)
 
 # --- Get Table 1 references ---
-unique_sources = (project_table
-                  .select("source_citation")
-                  .filter(pl.col("source_citation").is_not_null())
+source_final = (project_table
+                  .select("citation_short", "citation_long", "citation_url")
+                  .filter(pl.col("citation_short").is_not_null())
                   .unique()
-                  .to_series()
-                  .to_list())
-
-# Create placeholders for SQL query
-query_placeholders = ",".join(["%s"] * len(unique_sources))
-
-# Construct the query string
-query_sources = (f"SELECT citation_short, citation_long, citation_url FROM citations WHERE citation_short IN "
-                 f"({query_placeholders})")
-
-# Execute query
-cursor.execute(query_sources, unique_sources)
-records = cursor.fetchall()
-column_names = [desc[0] for desc in cursor.description]
-source_table = pl.DataFrame(records, schema=column_names, orient="row")
-
-# Update URL for LANDFIRE reference (use web page instead of direct download link)
-source_final = source_table.with_columns(pl.when(pl.col("citation_short") == "LANDFIRE 2016")
+                  .with_columns(pl.when(pl.col("citation_short") == "LANDFIRE 2016")
                                          .then(pl.lit(
                                                "https://landfire.gov/reference/lfrdb/lfrdb_data"))
                                          .otherwise(pl.col("citation_url"))
                                          .alias("citation_url")
-                                         )
+                                )
+                .with_columns(pl.concat_str(pl.col("citation_long"),
+                                            pl.col("citation_url"),
+                                            separator=" ")
+                              .alias("citation_full"))
+                .sort(by="citation_short")
+                .select("citation_short", "citation_full")
+                )
 
 # --- Create Table 1 --- #
 project_final = (project_table.lazy()
@@ -96,27 +84,26 @@ project_final = (project_table.lazy()
                                                    separator=" "))
     .otherwise(pl.col("project_code"))
     .alias("project_code"),
-    # Combine source_type and source_citation columns
-    pl.when(pl.col("source_citation").is_null())
+    # Combine source_type and Author-Year citation columns
+    pl.when(pl.col("citation_short").is_null())
     .then(pl.col("source_type"))
     # Add "partially published" caveat for ACCS Nuyakuk 2019 project
     .when(pl.col("project_code") == "accs_nuyakuk_2019")
-    .then(pl.concat_str(pl.col("source_citation"),
+    .then(pl.concat_str(pl.col("citation_short"),
                         pl.lit("(partially published)"),
                         separator=" "))
-    .otherwise(pl.col("source_citation"))
-    .alias("source_citation")
+    .otherwise(pl.col("citation_short"))
+    .alias("citation_short")
 )
                  # Select final columns
-                 .select(["project_code", "project_name", "temporal_extent", "source_citation"])
+                 .select(["project_code", "project_name", "temporal_extent", "citation_short"])
 
                  .collect()
                  )
 
-
-# Export table
+# --- Export Tables ---
 project_final.write_csv(project_output)
+source_final.write_csv(source_output)
 
 # Close the database connection
-cursor.close()
 akveg_db_connection.close()
