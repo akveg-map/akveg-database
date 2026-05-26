e2594cce034852c623f243119dcabda7b613705c
Amanda Droghini <10262153+adroghini@users.noreply.github.com>
2026-05-26 13:00:41 -0800

    Create taxon scope plot

--- data_summary/figure_temporal_distribution.py
+++ data_summary/figure_temporal_distribution.py
@@ -29,7 +29,8 @@ akveg_credentials = (credential_folder / "akveg_private_read" / "authentication_
 
 # Define outputs
 counts_output =  manuscript_folder / 'Tables' / 'table_record_counts.csv'
-plot_output = manuscript_folder / 'Figures' / 'figure_temporal_extent.png'
+year_output = manuscript_folder / 'Figures' / 'figure_temporal_extent.png'
+scope_output = manuscript_folder / 'Figures' / 'figure_taxon_scope.png'
 
 # Connect to the AKVEG Database
 akveg_db_connection = connect_database_postgresql(akveg_credentials)
@@ -59,13 +60,13 @@ counts_df = pl.DataFrame(records, schema=["table_name", "row_count"], orient="ro
 # --- Plot temporal range of data --- #
 
 # Query site visit table
-visit_date_query = f"""
+visit_query = f"""
 SELECT site_visit_code, 
 observe_date, 
 perspective.perspective,
-scope_vascular.scope AS scope_vascular,
-scope_bryophyte.scope AS scope_bryophyte,
-scope_lichen.scope AS scope_lichen
+scope_vascular.scope AS vascular,
+scope_bryophyte.scope AS bryophyte,
+scope_lichen.scope AS lichen
 FROM site_visit 
 LEFT JOIN site ON site_visit.site_code = site.site_code 
 LEFT JOIN perspective ON site.perspective_id = perspective.perspective_id
@@ -73,12 +74,12 @@ LEFT JOIN scope AS scope_vascular ON site_visit.scope_vascular_id = scope_vascul
 LEFT JOIN scope AS scope_bryophyte ON site_visit.scope_bryophyte_id = scope_bryophyte.scope_id
 LEFT JOIN scope AS scope_lichen ON site_visit.scope_lichen_id = scope_lichen.scope_id
 """
-visit_date_df = query_to_dataframe(akveg_db_connection, visit_date_query)
-visit_date_df = pl.from_pandas(visit_date_df)
+visit_df = query_to_dataframe(akveg_db_connection, visit_query)
+visit_df = pl.from_pandas(visit_df)
 
 # Bin data by 5-year intervals
 ## Except for pre-2000 data, which accounts for less than 2% of the total records
-visit_date_df = (visit_date_df.with_columns(pl.col("observe_date").dt.year().alias("observe_year")
+visit_df = (visit_df.with_columns(pl.col("observe_date").dt.year().alias("observe_year")
                                             )
                  .with_columns(pl.when(pl.col("observe_year") < 2000)
                                .then(pl.lit("Pre-2000"))
@@ -92,12 +93,12 @@ visit_date_df = (visit_date_df.with_columns(pl.col("observe_date").dt.year().ali
                  )
 
 # Calculate total number of site visits per time bin and perspective
-year_data = visit_date_df.group_by('year_interval', 'perspective').len(name="record_count")
+year_data = visit_df.group_by('year_interval', 'perspective').len(name="record_count")
 
 # Define sort order for plotting
 interval_order = ["Pre-2000", "2000-2004", "2005-2009", "2010-2014", "2015-2019", "2020-2024", "2025-2029"]
 
-# Create year-range plot with clustered bars for each perspective
+# Create year plot with clustered bars for each perspective
 year_plot = px.bar(
     year_data,
     x='year_interval',
@@ -135,15 +136,78 @@ year_plot.update_layout(
     )
 )
 
-# Show plot
+# Prepare data for taxonomic scope plot
+scope_mapping = {
+    "exhaustive": "Exhaustive",
+    "partial": "Partial",
+    "category": "Partial",
+    "top canopy": "Dominant Only",
+    "common species": "Dominant Only",
+    "high cover species": "Dominant Only",
+    "non-trace species": "Dominant Only",
+    "none": "Not Sampled"
+}
+scope_data = (visit_df.select(["site_visit_code", "vascular", "bryophyte", "lichen"])
+              .unpivot(index="site_visit_code",
+                       variable_name="taxon_group",
+                       value_name="scope_type")
+              .with_columns(pl.col("scope_type").str.replace_many(scope_mapping)
+                            .alias("scope_type_reclass")
+                            )
+              )
+
+# Calculate total number of site visits per scope type
+scope_data = scope_data.group_by('taxon_group', 'scope_type_reclass').len(name="record_count")
+
+# Define sort order
+taxon_order = ["vascular", "bryophyte", "lichen"]
+scope_order = ["Exhaustive", "Dominant Only", "Partial", "Not Sampled"]
+
+# Create plot with clustered bars for each taxonomic scope
+scope_plot = px.bar(
+    scope_data,
+    x='taxon_group',
+    y='record_count',
+    color='scope_type_reclass',
+    barmode='group',
+    pattern_shape='scope_type_reclass',
+    pattern_shape_sequence=['', '/', '\\', '.'],
+    color_discrete_sequence=['#252525', '#636363', '#b0b0b0', '#f0f0f0'],
+    category_orders={"taxon_group": taxon_order,
+                     "scope_type_reclass": scope_order},
+    template="plotly_white",
+    labels={"taxon_group": "Taxonomic Group", "record_count": "Number of Site Visits", "scope_type": "Taxonomic Scope"}
+)
+
+# Add same aesthetic modifications as year_plot
+scope_plot.update_traces(marker_line_color='black', marker_line_width=1)
+scope_plot.update_traces(texttemplate='%{y:,.0f}', textposition='outside')
+scope_plot.update_layout(
+    yaxis_tickformat=',',  # Update y-axis to use thousands separator
+    font=dict(
+        family="Arial",
+        size=20
+    ),
+    legend=dict(
+        orientation="h",
+        yanchor="bottom",
+        y=1.05,
+        xanchor="center",
+        x=0.5
+    )
+)
+
+# Show plots
 year_plot.show()
+scope_plot.show()
 
 # --- Export outputs ---
 # Export count table
 counts_df.write_csv(counts_output)
 
-# Export year range plot to PNG
-pio.write_image(year_plot, plot_output, width=1300, height=800, scale=3)
+# Export plots to PNG
+pio.write_image(year_plot, year_output, width=1300, height=800, scale=3)
+pio.write_image(scope_plot, scope_output, width=1300, height=800, scale=3)
 
 # --- Close DB connection ---
 cursor.close()
