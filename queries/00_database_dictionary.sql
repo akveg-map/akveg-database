777e6b22ac8d3bfd9bd0abd6cf7c2e4194b9bd82
Amanda Droghini <10262153+adroghini@users.noreply.github.com>
2026-06-02 15:53:56 -0800

    Create 00_database_dictionary.sql

---
+++ queries/00_database_dictionary.sql
@@ -0,0 +1,16 @@
+-- -*- coding: utf-8 -*-
+-- ---------------------------------------------------------------------------
+-- Query database dictionary
+-- Author: Amanda Droghini, Alaska Center for Conservation Science
+-- Last Updated: 2026-06-02
+-- Usage: Script should be executed in a PostgreSQL 17+ database.
+-- Description: "Query database dictionary" queries the database dictionary table, joining foreign key fields with their respective reference tables.
+-- ---------------------------------------------------------------------------
+
+-- Compile database schema
+SELECT database_schema.field as field
+     , database_dictionary.data_attribute as data_attribute
+     , database_dictionary.definition as definition
+FROM database_dictionary
+    LEFT JOIN database_schema ON database_dictionary.field_id = database_schema.field_id
+ORDER BY field, database_dictionary.data_attribute;
