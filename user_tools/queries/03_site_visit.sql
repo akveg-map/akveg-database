d5a070fd581dedfc2a1e64078f598b53a4867c1c
Amanda Droghini <10262153+adroghini@users.noreply.github.com>
2026-07-09 16:19:28 -0800

    Align fields with original table
    - Remove fields such as latitude_dd and longitude_dd that belong to the site table
    - Add observer and recorder fields that are in the original site visit table
    - Structure of resulting table now matches exactly with the table that is in the database

--- user_tools/queries/03_site_visit.sql
+++ user_tools/queries/03_site_visit.sql
@@ -2,7 +2,7 @@
 -- ---------------------------------------------------------------------------
 -- Query site visits
 -- Author: Timm Nawrocki, Alaska Center for Conservation Science
--- Last Updated: 2025-03-12
+-- Last Updated: 2026-07-09
 -- Usage: Script should be executed in a PostgreSQL 14+ database.
 -- Description: "Query site visits" queries all site visit data from the AKVEG Database.
 -- ---------------------------------------------------------------------------
@@ -15,12 +15,11 @@ SELECT site_visit.site_visit_code as site_visit_code
 	 , scope_vascular.scope as scope_vascular
      , scope_bryophyte.scope as scope_bryophyte
      , scope_lichen.scope as scope_lichen
-	 , perspective.perspective as perspective
-	 , cover_method.cover_method as cover_method
-	 , plot_dimensions.plot_dimensions_m as plot_dimensions_m
-	 , site_visit.observe_date as observe_date
-     , site.latitude_dd as latitude_dd
-     , site.longitude_dd as longitude_dd
+     , site_visit.observe_date as observe_date
+     , veg_observer.personnel as veg_observer
+     , veg_recorder.personnel as veg_recorder
+     , env_observer.personnel as env_observer
+     , soils_observer.personnel as soils_observer
      , structural_class.structural_class as structural_class
      , site_visit.homogeneous as homogeneous
 FROM site_visit
@@ -29,7 +28,8 @@ FROM site_visit
 	LEFT JOIN scope scope_vascular ON site_visit.scope_vascular_id = scope_vascular.scope_id
     LEFT JOIN scope scope_bryophyte ON site_visit.scope_bryophyte_id = scope_bryophyte.scope_id
     LEFT JOIN scope scope_lichen ON site_visit.scope_lichen_id = scope_lichen.scope_id
-	LEFT JOIN perspective ON site.perspective_id = perspective.perspective_id
-	LEFT JOIN cover_method ON site.cover_method_id = cover_method.cover_method_id
-	LEFT JOIN plot_dimensions ON site.plot_dimensions_id = plot_dimensions.plot_dimensions_id
+    LEFT JOIN personnel veg_observer ON site_visit.veg_observer_id = veg_observer.personnel_id
+    LEFT JOIN personnel veg_recorder ON site_visit.veg_recorder_id = veg_recorder.personnel_id
+    LEFT JOIN personnel env_observer ON site_visit.env_observer_id = env_observer.personnel_id
+    LEFT JOIN personnel soils_observer ON site_visit.soils_observer_id = soils_observer.personnel_id
     LEFT JOIN structural_class ON site_visit.structural_class_code = structural_class.structural_class_code;
