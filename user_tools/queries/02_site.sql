aa3dd454208a29eeea69b67e5846cd8f66febcdd
Amanda Droghini <10262153+adroghini@users.noreply.github.com>
2026-07-09 16:34:54 -0800

    Reorder fields  to match original table

--- user_tools/queries/02_site.sql
+++ user_tools/queries/02_site.sql
@@ -2,7 +2,7 @@
 -- ---------------------------------------------------------------------------
 -- Query sites
 -- Author: Timm Nawrocki, Alaska Center for Conservation Science
--- Last Updated:  2023-04-04
+-- Last Updated:  2026-07-09
 -- Usage: Script should be executed in a PostgreSQL 14+ database.
 -- Description: "Query sites" queries the site data.
 -- ---------------------------------------------------------------------------
@@ -12,12 +12,12 @@ SELECT site.site_code as site_code
      , site.establishing_project_code as establishing_project_code
      , perspective.perspective as perspective
      , cover_method.cover_method as cover_method
+     , plot_dimensions.plot_dimensions_m as plot_dimensions_m
      , h_datum.h_datum as h_datum
      , site.latitude_dd as latitude_dd
      , site.longitude_dd as longitude_dd
      , site.h_error_m as h_error_m
      , positional_accuracy.positional_accuracy as positional_accuracy
-     , plot_dimensions.plot_dimensions_m as plot_dimensions_m
      , location_type.location_type as location_type
 FROM site
     LEFT JOIN perspective ON site.perspective_id = perspective.perspective_id
