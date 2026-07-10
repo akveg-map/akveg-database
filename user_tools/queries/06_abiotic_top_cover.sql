9acf3c195f90dc2f3e41d67f1639b12768d86c6a
Amanda Droghini <10262153+adroghini@users.noreply.github.com>
2026-07-10 11:46:05 -0800

    Fix name of cover percent field
    Keep name as it is in the original database table

--- user_tools/queries/06_abiotic_top_cover.sql
+++ user_tools/queries/06_abiotic_top_cover.sql
@@ -2,7 +2,7 @@
 -- ---------------------------------------------------------------------------
 -- Query abiotic top cover
 -- Author: Timm Nawrocki, Alaska Center for Conservation Science
--- Last Updated: 2026-07-09
+-- Last Updated: 2026-07-10
 -- Usage: Script should be executed in a PostgreSQL 14+ database.
 -- Description: "Query abiotic top cover" queries all abiotic top cover data from the AKVEG Database.
 -- ---------------------------------------------------------------------------
@@ -10,6 +10,6 @@
 -- Compile abiotic top cover data
 SELECT abiotic_top_cover.site_visit_code as site_visit_code
      , ground_element.ground_element as abiotic_element
-     , abiotic_top_cover.abiotic_top_cover_percent as cover_percent
+     , abiotic_top_cover.abiotic_top_cover_percent as abiotic_top_cover_percent
 FROM abiotic_top_cover
     LEFT JOIN ground_element ON abiotic_top_cover.abiotic_element_code = ground_element.ground_element_code;
--- user_tools/queries/08_ground_cover.sql
+++ user_tools/queries/08_ground_cover.sql
@@ -2,7 +2,7 @@
 -- ---------------------------------------------------------------------------
 -- Query ground cover
 -- Author: Timm Nawrocki, Alaska Center for Conservation Science
--- Last Updated: 2026-07-09
+-- Last Updated: 2026-07-10
 -- Usage: Script should be executed in a PostgreSQL 14+ database.
 -- Description: "Query ground cover" queries the ground cover data.
 -- ---------------------------------------------------------------------------
@@ -10,6 +10,6 @@
 -- Compile ground cover data
 SELECT ground_cover.site_visit_code as site_visit_code
      , ground_element.ground_element as ground_element
-     , ground_cover.ground_cover_percent as cover_percent
+     , ground_cover.ground_cover_percent as ground_cover_percent
 FROM ground_cover
     LEFT JOIN ground_element ON ground_cover.ground_element_code = ground_element.ground_element_code;
