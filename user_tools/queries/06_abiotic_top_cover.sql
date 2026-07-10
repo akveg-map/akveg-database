984cfd68a14c5d27039d4a7c8208ddb3b4df760c
Amanda Droghini <10262153+adroghini@users.noreply.github.com>
2026-07-09 16:59:55 -0800

    Drop unnecessary left join

--- user_tools/queries/06_abiotic_top_cover.sql
+++ user_tools/queries/06_abiotic_top_cover.sql
@@ -2,7 +2,7 @@
 -- ---------------------------------------------------------------------------
 -- Query abiotic top cover
 -- Author: Timm Nawrocki, Alaska Center for Conservation Science
--- Last Updated: 2025-03-12
+-- Last Updated: 2026-07-09
 -- Usage: Script should be executed in a PostgreSQL 14+ database.
 -- Description: "Query abiotic top cover" queries all abiotic top cover data from the AKVEG Database.
 -- ---------------------------------------------------------------------------
@@ -12,5 +12,4 @@ SELECT abiotic_top_cover.site_visit_code as site_visit_code
      , ground_element.ground_element as abiotic_element
      , abiotic_top_cover.abiotic_top_cover_percent as cover_percent
 FROM abiotic_top_cover
-    LEFT JOIN site_visit ON abiotic_top_cover.site_visit_code = site_visit.site_visit_code
     LEFT JOIN ground_element ON abiotic_top_cover.abiotic_element_code = ground_element.ground_element_code;
