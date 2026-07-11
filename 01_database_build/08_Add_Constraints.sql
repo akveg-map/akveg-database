e8cbc41420bd42b92255fe86a6e16ccf79a6a9eb
Amanda Droghini <10262153+adroghini@users.noreply.github.com>
2026-07-10 19:34:51 -0800

    Create check constraint for missing value fields

--- 01_database_build/08_Add_Constraints.sql
+++ 01_database_build/08_Add_Constraints.sql
@@ -2,7 +2,7 @@
 -- ---------------------------------------------------------------------------
 -- Define check constraints
 -- Author: Amanda Droghini, Alaska Center for Conservation Science
--- Last Updated: 2026-06-25
+-- Last Updated: 2026-07-10
 -- Usage: Script should be executed in a PostgreSQL 17+ database.
 -- Description: "Define check constraints" alters existing tables by defining and adding a check constraint to validate inserted data. This script must be run after the INSERT scripts to function properly.
 -- ---------------------------------------------------------------------------
@@ -33,12 +33,20 @@ ALTER TABLE project
         CHECK ((completion_id = 1 AND year_end != -999) OR
                (completion_id = 2 AND year_end = -999));
 
+--- Add constraint to database schema table and validate records
+ALTER TABLE database_schema
+    ADD CONSTRAINT check_missing_values
+        CHECK (missing_value_code_id IS NULL OR missing_value_description IS NOT NULL);
+
 -- Remove constraints to avoid issues with export file
 ALTER TABLE abiotic_top_cover
     DROP CONSTRAINT check_abiotic_element_type;
 
 ALTER TABLE project
     DROP CONSTRAINT check_completion_date;
+
+ALTER TABLE database_schema
+    DROP CONSTRAINT check_missing_values;
 
 -- Commit transaction
 COMMIT TRANSACTION;
