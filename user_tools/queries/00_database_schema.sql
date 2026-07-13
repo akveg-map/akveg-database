d57b04e9320141b445dbbef828f34fb73a422c0c
Amanda Droghini <10262153+adroghini@users.noreply.github.com>
2026-07-12 16:10:41 -0800

    Add newly created fields
    Include missing_value_code and missing_value_description, update field name of is_required field

--- user_tools/queries/00_database_schema.sql
+++ user_tools/queries/00_database_schema.sql
@@ -2,7 +2,7 @@
 -- ---------------------------------------------------------------------------
 -- Query database schema
 -- Author: Amanda Droghini, Alaska Center for Conservation Science
--- Last Updated: 2026-06-02
+-- Last Updated: 2026-07-12
 -- Usage: Script should be executed in a PostgreSQL 17+ database.
 -- Description: "Query database schema" queries the database schema table, joining foreign key fields with their respective reference table.
 -- ---------------------------------------------------------------------------
@@ -17,12 +17,15 @@ SELECT database_schema.standards_section as standards_section
      , database_schema.is_unique as is_unique
 	 , database_schema.is_primary_key as is_primary_key
 	 , database_schema.is_foreign_key as is_foreign_key
-     , database_schema.required as required
+     , database_schema.is_required as is_required
      , link_table.schema_table as link_table
      , database_schema.field_description as field_description
+     , missing_value_code.missing_value_code as missing_value_code
+     , database_schema.missing_value_description as missing_value_description
 FROM database_schema
     LEFT JOIN schema_category ON database_schema.schema_category_id = schema_category.schema_category_id
 	LEFT JOIN schema_table ON database_schema.schema_table_id = schema_table.schema_table_id
     LEFT JOIN data_type ON database_schema.data_type_id = data_type.data_type_id
 	LEFT JOIN schema_table as link_table ON database_schema.link_table_id = link_table.schema_table_id
+    LEFT JOIN missing_value_code ON database_schema.missing_value_code_id = missing_value_code.missing_value_code_id
 ORDER BY database_schema.standards_section, schema_table, database_schema.field;
