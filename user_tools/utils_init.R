2d8322934541c92e6b40b60e9265fa1e156f3b88
Amanda Droghini <10262153+adroghini@users.noreply.github.com>
2026-08-02 11:43:26 -0800

    Parse additional file paths

--- user_tools/utils_init.R
+++ user_tools/utils_init.R
@@ -14,18 +14,22 @@
 load_system_paths <- function(config_file = "paths.yaml") {
   # Read the YAML file
   raw_config <- config::get(file = config_file)
-  
+
   # Build the base paths
   drive_path <- fs::path(raw_config$drive)
   root_path <- fs::path(drive_path, raw_config$root_folder)
-  
+  cloud_path <- fs::path(root_path, raw_config$cloud_path)
+
   # Construct absolute system paths
   resolved_paths <- list(
-    root        = root_path,
-    repository  = fs::path(root_path, raw_config$repository_path),
-    archive     = fs::path(root_path, raw_config$archive_path),
-    credentials = fs::path(root_path, raw_config$authentication_file)
+    root = root_path,
+    cloud = cloud_path,
+    repository = fs::path(root_path, raw_config$repository_path),
+    archive = fs::path(root_path, raw_config$archive_path),
+    plots = fs::path(cloud_path, raw_config$plots_path),
+    taxonomy = fs::path(cloud_path, raw_config$taxonomy_path),
+    credentials = fs::path(cloud_path, raw_config$authentication_file)
   )
-  
+
   return(resolved_paths)
 }
